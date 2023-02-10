74c0eb9900070f908123eaa560bad59d8079eff8
abhishekra07 <abhira719@gmail.com>
2023-02-10 09:45:36 +0530

    Create index.js

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require('cors')
+const bodyParser = require('body-parser');
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+   if(err) console.log(err) 
+   else console.log("mongdb is connected");
+  }
+);
+
+app.use(cors({
+  origin: '*'
+}));
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
