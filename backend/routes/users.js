3f3e5a14a626dc4b93cccaca59de47b108308818
abhishekra07 <abhira719@gmail.com>
2023-02-11 11:09:31 +0530

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -27,4 +27,17 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+//get a user
+router.get("/", async (req, res) => {
+    const userId = req.query.userId;
+    const username = req.query.username;
+    try {
+        const user = userId ? await User.findById(userId) : await User.findOne({username: username});
+        const { password, updatedAt, ...other } = user._doc;
+        res.status(200).json(other);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
