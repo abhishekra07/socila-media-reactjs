fdf4c7e07b64ed5f1a2ab97ccce8580dcd28b8db
abhishekra07 <abhira719@gmail.com>
2023-02-11 11:06:10 +0530

    Create users.js

---
+++ backend/routes/users.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+
+//update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+    try {
+      
+      res.status(200).json("Account has been updated");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can update only your account!");
+  }
+});
+
+module.exports = router;
