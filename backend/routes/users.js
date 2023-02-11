7591527852e2eb6ce356e96772e6cf73f3b40c13
abhishekra07 <abhira719@gmail.com>
2023-02-11 11:07:26 +0530

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -14,7 +14,10 @@ router.put("/:id", async (req, res) => {
       }
     }
     try {
-      
+        const user = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body,
+        });
+        res.status(200).json("Account has been updated");
       res.status(200).json("Account has been updated");
     } catch (err) {
       return res.status(500).json(err);
