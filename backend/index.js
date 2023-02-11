3f5742bdf0d947a2401edcd052d753c45ee7e7ff
abhishekra07 <abhira719@gmail.com>
2023-02-11 11:19:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,20 +12,24 @@ const userRoute = require("./routes/users");
 dotenv.config();
 
 mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err) => {
-   if(err) console.log(err) 
-   else console.log("mongdb is connected");
-  }
+    process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    (err) => {
+        if (err) console.log(err)
+        else console.log("mongdb is connected");
+    }
 );
 
 app.use(cors({
-  origin: '*'
+    origin: '*'
 }));
 
 //middleware
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(helmet());
@@ -35,5 +39,5 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 app.listen(8800, () => {
-  console.log("Backend server is running!");
+    console.log("Backend server is running!");
 });
