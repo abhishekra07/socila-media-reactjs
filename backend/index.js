a18141058867b175f4f43f6d39f8f4a98a5416b9
abhishekra07 <abhira719@gmail.com>
2023-02-14 14:12:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,7 @@ app.use(morgan("common"));
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
     console.log("Backend server is running!");
